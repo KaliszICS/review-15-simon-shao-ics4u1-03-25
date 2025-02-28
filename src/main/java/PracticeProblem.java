5eac8ab4bcdea5fcaa8e9f2c16677660f91ea63e
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-02-28 20:01:35 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,55 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(goodString("abcdfedree"));
 	}
+
+public static int goodString(String word){
+int operatorCount = 0;
+int charIn1 = -1;
+int charIn2 = -1;
+for (int i =0; i<word.length();i++){
+	for (int j = word.length()-1; j>0;j--){
+		
+if (i == j){
+}
+	else if (word.charAt(i)==word.charAt(j)){
+charIn1 = i;
+charIn2 = j;
+i = 100;
+j = -100;
+}
+	}
+}
+if (charIn1!=-1 && charIn2!=-1){
+	operatorCount = charIn1+word.length()-charIn2-1;
+	charIn1 = -1;
+	charIn1 = -1;
+}
+else{
+	operatorCount = -1;
+}
+
+for (int i =word.length()-1; i>0;i--){
+	for (int j = 0; j<word.length();j++){
+		
+if (i == j){
+}
+	else if (word.charAt(i)==word.charAt(j)){
+charIn1 = j;
+charIn2 = i;
+i = -100;
+j = 100;
+}
+	}
+}
+if (charIn1!=-1 && charIn2!=-1){
+if (charIn1+word.length()-charIn2-1 < operatorCount){
+	operatorCount = charIn1+word.length()-charIn2-1;
+}
+}
+return operatorCount;
+}
+
 
 }
